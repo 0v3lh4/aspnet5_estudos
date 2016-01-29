27e3c5f2414923c4486978a0668baf245ce4340e
Hendrik Araujo Mourão <jhendrik@gmail.com>
2016-01-29 18:40:20 -0200

    Criação do projeto de estudos do aspnet5

---
+++ TheWorld/src/TheWorld/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using TheWorld.Options;
+using Microsoft.Extensions.OptionsModel;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TheWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppSettings _appSettings;
+        public HomeController(IOptions<AppSettings> appSettings)
+        {
+            this._appSettings = appSettings.Value;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["Settings"] = this._appSettings.Logging.LogLevel.Microsoft;
+            return View();
+        }
+    }
+}
---
+++ TheWorld/src/TheWorld/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using TheWorld.Options;
+using Microsoft.Extensions.Logging;
+
+namespace TheWorld
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json")
+                .AddJsonFile($"AppSettings.{env.EnvironmentName}.json");
+
+            Configuration = configBuilder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppSettings>(Configuration);
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIISPlatformHandler();
+
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
+            app.UseStatusCodePages();
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
